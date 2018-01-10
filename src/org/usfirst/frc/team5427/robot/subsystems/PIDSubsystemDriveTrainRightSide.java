cd26c8958d057b3b24a1d09966a30ba22a064513
Varsha Kumar <varsha.kumar@outlook.com>
2018-01-09 20:37:19 -0600

    PIDSubsystem for Right Side has more comments and code TODOs in it

--- src/org/usfirst/frc/team5427/robot/subsystems/PIDSubsystemDriveTrainRightSide.java
+++ src/org/usfirst/frc/team5427/robot/subsystems/PIDSubsystemDriveTrainRightSide.java
@@ -7,32 +7,60 @@
 
 package org.usfirst.frc.team5427.robot.subsystems;
 
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * An example subsystem.  You can replace me with your own Subsystem.
+/**@author Varsha
+ * Subsystem to control speed of right side of DriveTrain using a PID Controller
  */
 public class PIDSubsystemDriveTrainRightSide extends PIDSubsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
-	public PIDSubsystemDriveTrainRightSide(double p, double i, double d) {
+	public PIDSubsystemDriveTrainRightSide(double p, double i, double d,double setpoint) {
+		//TODO Requires Robot Drive Train
+		//TODO Requires Robot AHRS
 		super(p, i, d);
 		// TODO Auto-generated constructor stub
+		this.setSetpoint(setpoint);
 	}
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 	}
-
+	
+	/**Source: WPILib
+	   * Returns the input for the pid loop.
+	   *
+	   * <p>It returns the input for the pid loop, so if this command was based off of a gyro, then it
+	   * should return the angle of the gyro.
+	   *
+	   * <p>All subclasses of {@link PIDCommand} must override this method.
+	   *
+	   * <p>This method will be called in a different thread then the {@link Scheduler} thread.
+	   *
+	   * @return the value the pid loop should use as input
+	   */
 	@Override
 	protected double returnPIDInput() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**Source: WPILib
+	   * Uses the value that the pid loop calculated. The calculated value is the "output" parameter.
+	   * This method is a good time to set motor values, maybe something along the lines of
+	   * <code>driveline.tankDrive(output, -output)</code>
+	   *
+	   * <p>All subclasses of {@link PIDCommand} must override this method.
+	   *
+	   * <p>This method will be called in a different thread then the {@link Scheduler} thread.
+	   *
+	   * @param output the value the pid loop calculated
+	   */
 	@Override
 	protected void usePIDOutput(double output) {
 		// TODO Auto-generated method stub
