c0bf409d5198019818a17eb2f319330eb92234be
Blake Romero <blake.romero724@gmail.com>
2018-03-13 20:43:47 -0500

    Changed code for Right_ScaleIsRight to match Left_ScaleIsLeft

--- src/org/usfirst/frc/team5427/autoCommands/Right_ScaleIsRight.java
+++ src/org/usfirst/frc/team5427/autoCommands/Right_ScaleIsRight.java
@@ -7,48 +7,44 @@ import org.usfirst.frc.team5427.robot.commands.MoveElevatorAuto;
 import org.usfirst.frc.team5427.robot.commands.PIDStraightMovement;
 import org.usfirst.frc.team5427.robot.commands.PIDTurn;
 import org.usfirst.frc.team5427.util.Config;
+import org.usfirst.frc.team5427.util.SameLine;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
+@SameLine
 public class Right_ScaleIsRight extends AutoPath {
-	private PIDStraightMovement firstDistance,secondDistance;
+	private PIDStraightMovement firstDistance, secondDistance;
 	private PIDTurn fidgetSpinner,firstAngle;
 	private MoveElevatorAuto moveElevator;
 	private Fidget fidget;
-	
-	//the start and current time of the auto path in seconds
 	private double startTime, currentTime;
 	
-	//Times TODO: test for times
-	public static final double timeOut1 = 13;
-
-	//Values for 18 inches.
+	//Time
+//	public static final double timeOut1 = 0;
+	
+	// Values for 18 inches.
 	public static final double p1 = 0.011;
 	public static final double i1 = 0.0;
 	public static final double d1 = 0.018;
 	
-	public static final double p2 = 0.02;
+	public static final double p2 = 0.2;
 	public static final double i2 = 0.0;
-	public static final double d2 = 0.0;
-	
-	
+	public static final double d2 = 0.1;
+
 	public Right_ScaleIsRight() {
-		// creates all of the PID Commands
-		requires(Robot.driveTrain);
 		fidget = new Fidget();
-//		fidget = null;
 		fidgetSpinner = new PIDTurn(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, -18.5);
-		firstDistance = new PIDStraightMovement(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, Config.PID_STRAIGHT_POWER_LONG, 220, p1, i1, d1);
-		firstAngle = new PIDTurn(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, -40);
-		moveElevator = new MoveElevatorAuto(2); // 1 for switch
-//		secondDistance = new PIDStraightMovement(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, Config.PID_STRAIGHT_POWER_SHORT, 16, p2, i2, d2);
+		firstDistance = new PIDStraightMovement(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, Config.PID_STRAIGHT_POWER_LONG+.1, 250, p1, i1, d1);
+		firstAngle = new PIDTurn(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, -45);
+		secondDistance = new PIDStraightMovement(Robot.driveTrain.drive_Right, Robot.driveTrain.drive_Left, Config.PID_STRAIGHT_POWER_LONG, 25, p1, i1, d1);
+		moveElevator = new MoveElevatorAuto(2);
+		setTimeout(13.75);
 	}
 
 	// begins the command
 	public void initialize() {
 		startTime = System.nanoTime()/1000000000.;
-		fidget.start();		
-//		firstDistance.start();
+		fidget.start();
 	}
 
 	// uses the previous commands being null to check if a certain command needs to
@@ -58,7 +54,18 @@ public class Right_ScaleIsRight extends AutoPath {
 
 		if(moveElevator != null)
 			moveElevator.isFinished();
-		if (null == fidget && null == firstDistance && firstAngle !=null&& firstAngle.isFinished() && !(moveElevator.isRunning())) {
+		if (null == fidget && null == firstDistance && firstAngle ==null&& moveElevator.maxHeightReached() && (!secondDistance.isRunning())) {
+			System.out.println("Part 17 Done.");
+//			firstAngle.cancel();
+//			firstAngle = null;
+			Robot.ahrs.reset();
+			Robot.encLeft.reset();
+			secondDistance.start();
+//			Robot.encRight.reset();
+//			secondDistance.start();
+//			moveElevator.start();
+		}
+		else if (null == fidget && null == firstDistance && firstAngle !=null&& firstAngle.isFinished() && !(moveElevator.isRunning())) {
 			System.out.println("Part 1 Done.");
 			firstAngle.cancel();
 			firstAngle = null;
@@ -101,25 +108,15 @@ public class Right_ScaleIsRight extends AutoPath {
 	@Override
 	public boolean isFinished() {
 		// returns if the last distance has finished and the robot has shot the box
-		if (firstAngle == null && moveElevator.isFinished()) {
+		if (firstAngle == null && secondDistance.isFinished())
 			return true;
-		}
-		return false;
-		
-//		if (firstDistance != null)
-//			return firstDistance.isFinished();
-//		return false;
+		return isTimedOut();
 	}
-	
-	// @Override
-	// protected void end() {
-	// firstAngle.free();
-	// firstDisance
-	// }
-	
+
 	@Override
 	protected void end() {
-		new DriveBackward(0.5).start();
+		moveElevator.cancel();
 		super.end();
+		new DriveBackward(2).start();
 	}
 }
