56e3ec3c656dbdc7c1ac927c03cc9eeb98fc1fcb
Blake Romero <blake.romero724@gmail.com>
2018-04-17 20:49:10 -0500

    Commented Robot, AutoPath, and part of BaseLine_With_Delay

--- src/org/usfirst/frc/team5427/autoCommands/AutoPath.java
+++ src/org/usfirst/frc/team5427/autoCommands/AutoPath.java
@@ -5,14 +5,26 @@ import org.usfirst.frc.team5427.util.SameLine;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * This is the base for all of our autonomous paths.
+ * @author Blake
+ */
 @SameLine
 public class AutoPath extends Command {
 	
+	/**
+	 * This is run periodically to check to see if
+	 * the command is finished.
+	 */
 	@Override
 	public boolean isFinished() {
 		return false;
 	}
 
+	/**
+	 * This is run once when the command's is finished
+	 * returns true.
+	 */
 	@Override
 	protected void end() {
 		new AutoOutGo().start();
