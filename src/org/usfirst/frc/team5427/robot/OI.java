2c0faa9f84839659028defdd5c9b6c99f95bd38a
Blake Romero <blake.romero724@gmail.com>
2018-04-20 17:49:33 -0500

    Documented OI

--- src/org/usfirst/frc/team5427/robot/OI.java
+++ src/org/usfirst/frc/team5427/robot/OI.java
@@ -10,10 +10,6 @@ import org.usfirst.frc.team5427.robot.commands.IntakeActivateIn;
 import org.usfirst.frc.team5427.robot.commands.IntakeActivateOut;
 import org.usfirst.frc.team5427.robot.commands.IntakeActivateOutSlow;
 import org.usfirst.frc.team5427.robot.commands.ManualMoveElevatorDown;
-//import org.usfirst.frc.team5427.robot.commands.MoveClimberArmDown;
-//import org.usfirst.frc.team5427.robot.commands.MoveClimberArmUp;
-//import org.usfirst.frc.team5427.robot.commands.MoveClimberDown;
-//import org.usfirst.frc.team5427.robot.commands.MoveClimberUp;
 import org.usfirst.frc.team5427.robot.commands.MoveElevatorFull;
 import org.usfirst.frc.team5427.robot.commands.TiltIntakeDown;
 import org.usfirst.frc.team5427.robot.commands.TiltIntakeUp;
@@ -21,105 +17,148 @@ import org.usfirst.frc.team5427.robot.commands.TiltIntake_TimeOut;
 import org.usfirst.frc.team5427.robot.commands.TiltIntake_TimeOut;
 import org.usfirst.frc.team5427.util.Config;
 import org.usfirst.frc.team5427.util.SameLine;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  *
- * This file creates the joy stick and assigns functions to its buttons
- * This file will use the same line method
+ * This file creates the joy stick and assigns functions to its buttons.
+ * 
+ * @author Varsha Kumar
  */
 @SameLine
 public class OI {
 
+	/**
+	 * This is the joystick that we use to control the robot's movements and
+	 * actions.
+	 */
 	public Joystick joy1;
+
+	/******************** BUTTONS ********************/
+	/**
+	 * Button used to pull cubes into the robot.
+	 */
 	Button motorIntakeIn;
+
+	/**
+	 * Button used to shoot cubes out of the robot.
+	 */
 	Button motorIntakeOut;
+
+	/**
+	 * Button used to move the elevator upwards.
+	 */
 	Button elevatorUp;
+
+	/**
+	 * Button used to move the elevator downwards.
+	 */
 	Button elevatorDown;
+
+	/**
+	 * Button used to move the climber arm upwards.
+	 */
 	Button climberArmUp;
+
+	/**
+	 * Button used to move the climber arm downwards.
+	 */
 	Button climberArmDown;
+
+	/**
+	 * Button used to switch the tilted position of the intake.
+	 */
 	Button intakeTilterToggle;
+
+	/**
+	 * Button used to manually tilt the intake upwards.
+	 */
 	Button intakeTilterUp;
+
+	/**
+	 * Button used to manually tilt the intake downwards.
+	 */
 	Button intakeTilterDown;
+
+	/**
+	 * Button used to climb upwards.
+	 */
 	Button climberUp;
+
+	/**
+	 * Button used to climb downwards.
+	 */
 	Button climberDown;
- 
+
+	/**
+	 * Button used to shoot cubes out of the robot slowly.
+	 */
 	Button softOutGo;
+	/*************************************************/
 
-
-	SendableChooser<Integer> autoColorChooser = new SendableChooser<Integer>();
+	/**
+	 * Chooser used to select the robot's autonomous position.
+	 */
 	SendableChooser<Integer> autoPositionChooser = new SendableChooser<Integer>();
+
+	/**
+	 * Chooser used to select the robot's desired cube placement.
+	 */
 	SendableChooser<Integer> autoCubeChooser = new SendableChooser<Integer>();
-	
+
+	/**
+	 * Constructor of OI used to initalize all of the buttons to their joystick
+	 * locations and assign each button to a specific command, as well as create the
+	 * autonomous choosers on the SmartDashboard.
+	 */
 	public OI() {
 		joy1 = new Joystick(Config.JOYSTICK_PORT);
-        
-        /** 
-         * Create the buttons
-         */
-        motorIntakeIn = new JoystickButton(joy1,Config.BUTTON_MOTOR_INTAKE_IN);
-        motorIntakeOut = new JoystickButton(joy1, Config.BUTTON_MOTOR_INTAKE_OUT);
-        elevatorUp = new JoystickButton(joy1,Config.BUTTON_ELEVATOR_UP);
-        elevatorDown = new JoystickButton(joy1,Config.BUTTON_ELEVATOR_DOWN);
-        softOutGo =  new JoystickButton(joy1,Config.BUTTON_MOTOR_INTAKE_OUT_SLOW);
-        climberArmDown = new JoystickButton(joy1,Config.BUTTON_ELEVATOR_DOWN_MANUAL);
-        intakeTilterToggle = new JoystickButton(joy1,Config.BUTTON_INTAKE_TOGGLE_TILTER);
-        intakeTilterUp = new JoystickButton(joy1,Config.BUTTON_INTAKE_TILTER_UP);
-        intakeTilterDown = new JoystickButton(joy1,Config.BUTTON_INTAKE_TILTER_DOWN);
 
-        
-        /**
-         * Sets the functions of the buttons
-         */
-        motorIntakeIn.whileHeld(new IntakeActivateIn());
-        motorIntakeOut.whileHeld(new IntakeActivateOut());
-        
-        elevatorUp.whileHeld(Robot.mou);
-        elevatorDown.whileHeld(Robot.mod);
-        
-        elevatorUp.toggleWhenPressed(Robot.mou);
-        elevatorDown.toggleWhenPressed(Robot.mod);
-        
-        
-        intakeTilterToggle.whenPressed(new TiltIntake_TimeOut());
-        intakeTilterUp.whenPressed(new TiltIntakeUp());
-        intakeTilterDown.whenPressed(new TiltIntakeDown());
+		motorIntakeIn = new JoystickButton(joy1, Config.BUTTON_MOTOR_INTAKE_IN);
+		motorIntakeOut = new JoystickButton(joy1, Config.BUTTON_MOTOR_INTAKE_OUT);
+		elevatorUp = new JoystickButton(joy1, Config.BUTTON_ELEVATOR_UP);
+		elevatorDown = new JoystickButton(joy1, Config.BUTTON_ELEVATOR_DOWN);
+		softOutGo = new JoystickButton(joy1, Config.BUTTON_MOTOR_INTAKE_OUT_SLOW);
+		climberArmDown = new JoystickButton(joy1, Config.BUTTON_ELEVATOR_DOWN_MANUAL);
+		intakeTilterToggle = new JoystickButton(joy1, Config.BUTTON_INTAKE_TOGGLE_TILTER);
+		intakeTilterUp = new JoystickButton(joy1, Config.BUTTON_INTAKE_TILTER_UP);
+		intakeTilterDown = new JoystickButton(joy1, Config.BUTTON_INTAKE_TILTER_DOWN);
 
-        
-        climberArmDown.whileHeld(new ManualMoveElevatorDown());
-        
-        autoPositionChooser.addDefault("CHOOSE ONE", Config.AUTO_NONE);
+		motorIntakeIn.whileHeld(new IntakeActivateIn());
+		motorIntakeOut.whileHeld(new IntakeActivateOut());
+		elevatorUp.whileHeld(Robot.mou);
+		elevatorDown.whileHeld(Robot.mod);
+		elevatorUp.toggleWhenPressed(Robot.mou);
+		elevatorDown.toggleWhenPressed(Robot.mod);
+		intakeTilterToggle.whenPressed(new TiltIntake_TimeOut());
+		intakeTilterUp.whenPressed(new TiltIntakeUp());
+		intakeTilterDown.whenPressed(new TiltIntakeDown());
+		climberArmDown.whileHeld(new ManualMoveElevatorDown());
+
+		autoPositionChooser.addDefault("CHOOSE ONE", Config.AUTO_NONE);
 		autoPositionChooser.addObject("Right", Config.RIGHT);
 		autoPositionChooser.addObject("Center", Config.CENTER);
 		autoPositionChooser.addObject("Left", Config.LEFT);
-		
 		autoCubeChooser.addDefault("CHOOSE ONE", Config.AUTO_NONE);
 		autoCubeChooser.addObject("Switch", Config.SWITCH);
-		autoCubeChooser.addObject("Scale", Config.SCALE);	
-		
-		SmartDashboard.putData("Field Position",autoPositionChooser);
-		SmartDashboard.putData("Cube Placement",autoCubeChooser);
-		SmartDashboard.putString("Position Selector", "Position Selector");
-		SmartDashboard.putString("Cube Placement Selector", "Cube Placement Selector");
-		SmartDashboard.putString("Intake Camera", "Intake Camera");
-    }
+		autoCubeChooser.addObject("Scale", Config.SCALE);
+		SmartDashboard.putData("Field Position", autoPositionChooser);
+		SmartDashboard.putData("Cube Placement", autoCubeChooser);
+	}
 
-	
 	/**
-	 * Gets the name of the joy stick being used
-	 * @return this joy stick's name
+	 * Accessor for the joystick we use.
+	 * 
+	 * @return the current joystick.
 	 */
-    public Joystick getJoy() {
-        return joy1;
-    } 
-	    
+	public Joystick getJoy() {
+		return joy1;
+	}
+
 }
